e682fb33025803632b3b868bfe9b2f8c7952e35a
MichaelGallinago <michael.gallinago@gmail.com>
2024-12-21 01:11:12 +0500

    Sort fix

--- src/components/TodoItems/TodoItems.jsx
+++ src/components/TodoItems/TodoItems.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import {TodoItemsContainer} from './TodoItemsContainer';
 import {NewTodoItem} from '../TodoItem/NewTodoItem';
 import {TodoItem} from '../TodoItem/TodoItem';
@@ -23,20 +23,24 @@ const SortButton = styled.button`
 
 export const TodoItems = () => {
     const [searchValue, setSearchValue] = useState('');
-    const [sortedItems, setSortedItems] = useState(null);
+    const [isSorted, setIsSorted] = useState(false);
 
     const {data: todoItems, isLoading} = useData();
 
-    useEffect(() => {
-        if (sortedItems) {
-            const filteredBySearchItems = todoItems.filter((todoItem) => {
-                const clearedItemTitle = todoItem.title.replace(/\s+/g, '').toLowerCase();
-                const clearedSearchValue = searchValue.replace(/\s+/g, '').toLowerCase();
-                return clearedItemTitle.includes(clearedSearchValue) || clearedSearchValue.length < 1;
-            });
-            setSortedItems(filteredBySearchItems.sort((a, b) => b.priority - a.priority));
+    const filteredBySearchItems = useMemo(() => {
+        if (!todoItems) return [];
+
+        const filteredItems = todoItems.filter((todoItem) => {
+            const clearedItemTitle = todoItem.title.replace(/\s+/g, '').toLowerCase();
+            const clearedSearchValue = searchValue.replace(/\s+/g, '').toLowerCase();
+            return clearedItemTitle.includes(clearedSearchValue) || clearedSearchValue.length < 1;
+        });
+
+        if (isSorted) {
+            return filteredItems.sort((a, b) => b.priority - a.priority);
         }
-    }, [todoItems, searchValue, sortedItems]);
+        return filteredItems;
+    }, [todoItems, searchValue, isSorted]);
 
     if (!todoItems || isLoading) {
         return (
@@ -46,41 +50,25 @@ export const TodoItems = () => {
         );
     }
 
-    const filteredBySearchItems = todoItems.filter((todoItem) => {
-        const clearedItemTitle = todoItem.title.replace(/\s+/g, '').toLowerCase();
-        const clearedSearchValue = searchValue.replace(/\s+/g, '').toLowerCase();
-        return clearedItemTitle.includes(clearedSearchValue) || clearedSearchValue.length < 1;
-    });
-
     const onClickHandler = () => {
-        if (sortedItems) {
-            setSortedItems(null)
-            return;
-        }
-        setSortedItems(filteredBySearchItems.sort((a, b) => b.priority - a.priority));
+        setIsSorted((prev) => !prev);
     };
 
-    const todoItemsElements = sortedItems ? sortedItems.map((item, index) => {
-        return <TodoItem
+    const todoItemsElements = filteredBySearchItems.map((item, index) => (
+        <TodoItem
             key={item.id}
             title={item.title}
             checked={item.isDone}
             id={item.id}
-            priority={item.priority}/>;
-    }) : filteredBySearchItems.map((item, index) => {
-        return <TodoItem
-            key={item.id}
-            title={item.title}
-            checked={item.isDone}
-            id={item.id}
-            priority={item.priority}/>;
-    });
+            priority={item.priority}
+        />
+    ));
 
     return (
         <TodoItemsContainer>
-            <SearchInput value={searchValue} setValue={setSearchValue} setSortedItems={setSortedItems} />
+            <SearchInput value={searchValue} setValue={setSearchValue}/>
             <SortButton onClick={onClickHandler}>
-                { sortedItems ? 'Не сортировать' : 'Отсортировать по приоритету' }
+                {isSorted ? 'Не сортировать' : 'Отсортировать по приоритету'}
             </SortButton>
             {todoItemsElements}
             <NewTodoItem />
